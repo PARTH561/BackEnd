dc652d9a0c0e1e3947a805e4841d437fc87dc557
Parth Thakur <157449214+PARTH561@users.noreply.github.com>
2025-02-24 20:03:27 +0530

    Update server.js

--- Post Request/server.js
+++ Post Request/server.js
@@ -1,18 +1,29 @@
 const express = require("express");
 const app = express();
-
-
+app.use(express.urlencoded({extended:true}))
+let userData = [];
 
 app.get("/adduser",(req,res)=>{
     res.sendFile(__dirname+"/register.html");
 })
 
 
+app.post("/adduser",(req,res)=>{
+    let { username,email,password} = req.body;
+    console.log(username,email,password);
 
+    let newUser = {
+        name : username,
+        email : email,
+        password : password
+    };
+    userData.push(newUSer);
+    res.send(userData);
+});
 
 
 
 
 app.listen(4670,()=>{
     console.log("server is running on port 4670");
-})
+});
